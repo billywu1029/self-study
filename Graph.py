2536a8b74ba2ceabd22fd63308b7a723e98b27e5
William Wu <billwu@mit.edu>
2019-08-12 21:30:27 -0700

    Added topological sort relaxation support

--- Graph.py
+++ Graph.py
@@ -41,6 +41,65 @@ class Graph:
     def addVertex(self, x):
         self.vertices.add(Vertex(x))
 
+    def SSSPTopologicalRelaxation(self, s):
+        """
+        Finds the single source shortest paths between the source s and all other vertices
+        The graph must be a DAG for the algorithm to produce the correct SSSP
+        :return: mapping of vertex pairs to their shortest path weight
+        """
+        self.verifyDAG(s)
+        shortestPaths = {s:0}  # maps vertex u -> d(s, u) for all vertices u
+        for v in self.vertices:
+            if v != s:
+                shortestPaths[v] = float('inf')
+
+        # list of graph's vertices reachable from s in topological order
+        verticesSorted = Topological_Sort.topological_sort_SS(self, s)
+        for u in verticesSorted:
+            if u in self.edges.keys():
+                for e in self.edges[u]:
+                    v, w = e.v, e.w
+                    self.relax(u, v, w, shortestPaths)
+        return shortestPaths
+
+    def relax(self, u, v, w, d):
+        """
+        If current "shortest" distance from s to v is greater than shortest distance from s to u + w(u,v), then set
+        the new shortest distance from s to v to be = newly found shortest distance d(s,u)+w(u,v).
+        Better explained by illustration:
+                   d(s,v)
+                s ~~~~~~> v
+                |      /
+                |     /
+         d(s,u) |    /
+                |   /  w(u,v)
+                |  /
+                | /
+                u
+        """
+        assert u in d and v in d and u in self.weights and v in self.weights[u]
+        assert w == self.weights[u][v]
+        if d[v] > d[u] + w:
+            d[v] = d[u] + w
+
+    def verifyDAG(self, s):
+        """
+        In order for the SSSP by topological sort to work, ensure that this graph is a directed acyclic graph (DAG),
+        for the nodes reachable by s
+        :param s specifying source node s to make traversal start easier
+        """
+        # Find presence of a back edge, if none, then the nodes reachable from s + edges traversed form a DAG
+        curr_nodes = set()
+        def traverse(root):
+            curr_nodes.add(root)
+            for child in self.getChildren(root):
+                if child in curr_nodes:
+                    assert False
+                traverse(child)
+            curr_nodes.remove(root)  # Important, to keep nodes in the stack updated
+
+        return traverse(s)
+
 
 class Vertex:
     # Assume that a Vertex is immutable
@@ -91,5 +150,34 @@ class Edge:
         else:
             return False
 
+if __name__ == "__main__":
+    a, b, c, d, e = Vertex("a"), Vertex("b"), Vertex("c"), Vertex("d"), Vertex("e")
+    g = Graph({a, b, c, d})
+    g.addEdge(a, b, 1)
+    g.addEdge(a, c, -1)
+    g.addEdge(b, d, 4)
+    g.addEdge(b, c, 2)
+    g.addEdge(c, d, 5)
+    g.addEdge(c, d, 5)
+    g.addEdge(a, e, 10)
+    g.addEdge(e, d, -7)
+
+    print("Edges: ", g.getEdges())
+    try:
+        print(g.SSSPTopologicalRelaxation(a))
+        print("successfully determined that graph g was a DAG")
+    except AssertionError:
+        print("wasn't supposed to fail...")
+
+    g2 = Graph({a, b, c, d})
+    g2.addEdge(a, b, 1)
+    g2.addEdge(a, c, -1)
+    g2.addEdge(d, a, 4)
+    g2.addEdge(c, d, 5)
+
+    try:
+        print(g2.SSSPTopologicalRelaxation(a))
+    except AssertionError:
+        print("successfully detected graph g2 was not a DAG")
 
 
--- Topological_Sort.py
+++ Topological_Sort.py
@@ -15,24 +15,29 @@ def topological_sort(graph):
     :return: list of graph vertices in topologically sorted order
     """
     result, visited, unvisited = [], set(), set(graph.keys())
-    def add_children_of(root):
-        """
-        Prepends the root node before its children in the result list based on DFS ordering
-        Updates visited and unvisited sets to reflect traversal through nodes under root
-        :param root: starting node of the traversal
-        """
-        visited.add(root)
-        for child in graph[root]:
-            if child not in visited:
-                add_children_of(child)  # recursive call may run into stack frame limit problems, consider making iterative
-
-        # prepend the node to the front of the topological ordering, in front of its children
-        result.insert(0, root)
-        unvisited.remove(root)
-
     while nodes_left_to_traverse(graph.keys(), visited):
-        add_children_of(next(iter(unvisited)))
+        add_children_of(graph, next(iter(unvisited)), result, visited, unvisited)
     return result
+
+def add_children_of(graph, root, result, visited, unvisited=None):
+    """
+    Prepends the root node before its children in the result list based on DFS ordering
+    Updates visited and unvisited sets to reflect traversal through nodes under root
+    :param graph: input graph
+    :param root: starting node of the traversal
+    :param result: list of nodes meant to be in topological order after this function call
+    :param visited: set of visited nodes
+    :param unvisited: optional to specify which nodes must still be traversed, potentially in a separate tree
+    """
+    visited.add(root)
+    for child in graph[root]:
+        if child not in visited:
+            add_children_of(graph, child, result, visited, unvisited)
+
+    # prepend the node to the front of the topological ordering, in front of its children
+    result.insert(0, root)
+    if unvisited is not None:
+        unvisited.remove(root)
 
 def nodes_left_to_traverse(all_nodes, visited_nodes):
     """
@@ -44,6 +49,17 @@ def nodes_left_to_traverse(all_nodes, visited_nodes):
     # Uncomment when debugging, ensure that no non-graph nodes are ever added to visited set
     # assert visitedNodes.issubset(allNodes)
     return len(all_nodes) != len(visited_nodes)
+
+def topological_sort_SS(graph, source):
+    """
+    Finds a topological sort ordering on a graph given a source node. Will exclude nodes unreachable from the source.
+    :param graph: input graph
+    :param source: input source
+    :return: list of nodes of the graph that are reachable from source in a topological ordering
+    """
+    result, visited = [source], {source}
+    add_children_of(graph, source, result, visited)
+    return result
 
 
 if __name__ == '__main__':

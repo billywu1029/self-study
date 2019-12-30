638b7ac4dc46b3fa963144b2341889c4a934f793
Bill Wu <billwu@mit.edu>
2019-12-30 00:47:01 -0500

    Dijkstra's Algorithm SSSP initial code

--- Graph.py
+++ Graph.py
@@ -1,3 +1,24 @@
+class Vertex:
+    # Assume that a Vertex is immutable
+    def __init__(self, x):
+        # Must have immutable value x
+        self.val = x
+
+    def copy(self):
+        return Vertex(self.val)
+
+    def __str__(self):
+        return "vertex %r" % self.val
+
+    def __repr__(self):
+        return "Vertex(%r)" % self.val
+
+    def __hash__(self):
+        return hash(self.val)
+
+    def __eq__(self, other):
+        return isinstance(other, Vertex) and other.val == self.val
+
 class Graph:
     def __init__(self, vertices=None, edges=None):
         # Can construct a graph via edges adjacency set (u -> {v1: w1, v2: w2}, ...})
@@ -59,44 +80,6 @@ class Graph:
     def addVertex(self, x):
         self.vertices.add(Vertex(x))
 
-    def relax(self, u, v, w, d):
-        """
-        If current "shortest" distance from s to v is greater than shortest distance from s to u + w(u,v), then set
-        the new shortest distance from s to v to be = newly found shortest distance d(s,u)+w(u,v).
-        Better explained by illustration:
-                   d(s,v)
-                s ~~~~~~> v
-                |      /
-                |     /
-         d(s,u) |    /
-                |   /  w(u,v)
-                |  /
-                | /
-                u
-        """
-        assert u in d and v in d and u in self.edges and v in self.edges[u]
-        assert w == self.edges[u][v]
-        if d[v] > d[u] + w:
-            d[v] = d[u] + w
-
-    def verifyDAG(self, s):
-        """
-        In order for the SSSP by topological sort to work, ensure that this graph is a directed acyclic graph (DAG),
-        for the nodes reachable by s
-        :param s specifying source node s to make traversal start easier
-        """
-        # Find presence of a back edge, if none, then the nodes reachable from s + edges traversed form a DAG
-        curr_nodes = set()
-        def traverse(root):
-            curr_nodes.add(root)
-            for child in self.getChildren(root):
-                if child in curr_nodes:
-                    assert False
-                traverse(child)
-            curr_nodes.remove(root)  # Important, to keep nodes in the stack updated
-
-        return traverse(s)
-
     def bfs(self, start, target):
         # Given a graph/adjacency matrix/adjacency set, (in 6.006 ex create dict of paths to all V) find SP to target
         queue, visited, parents = [start], {start}, {start: start}
@@ -149,28 +132,123 @@ class Graph:
 
         return path[::-1]  # Reverse path so that it is from start to target
 
+    # Should this be a static method or not??
+    def relax(self, u, v, d):
+        """
+        If current "shortest" distance from s to v is greater than shortest distance from s to u + w(u,v), then set
+        the new shortest distance from s to v to be = newly found shortest distance d(s,u) + w(u,v).
+        Return the boolean result from the triangle inequality condition mentioned above as well.
+        Better explained by illustration: d(s,v) <- d(s,u) + w(u,v) if d(s,v) > d(s,u) + w(u,v)
+                   d(s,v)
+                s ~~~~~~> v
+                |      /
+                |     /
+         d(s,u) |    /
+                |   /  w(u,v)
+                |  /
+                | /
+                u
+        """
+        assert u in d and v in d and u in self.edges and v in self.edges[u]
+        flag = d[v] > d[u] + self.edges[u][v]
+        if flag:
+            d[v] = d[u] + self.edges[u][v]
+        return flag
 
-class Vertex:
-    # Assume that a Vertex is immutable
-    def __init__(self, x):
-        # Must have immutable value x
-        self.val = x
+    def verifyDAG(self, s):
+        """
+        In order for the SSSP by topological sort to work, ensure that this graph is a directed acyclic graph (DAG),
+        for the nodes reachable by s
+        :param s specifying source node s to make traversal start easier
+        """
+        # Find presence of a back edge, if none, then the nodes reachable from s + edges traversed form a DAG
+        curr_nodes = set()
 
-    def copy(self):
-        return Vertex(self.val)
+        def traverse(root):
+            curr_nodes.add(root)
+            for child in self.getChildren(root):
+                if child in curr_nodes:
+                    assert False
+                traverse(child)
+            curr_nodes.remove(root)  # Important, to keep nodes in the stack updated
 
-    def __str__(self):
-        return "vertex %r" % self.val
+        return traverse(s)
 
-    def __repr__(self):
-        return "Vertex(%r)" % self.val
+    @staticmethod
+    def minDistVertex(pq, d):
+        """
+        Find the vertex with current minimum distance and update Q to reflect its removal from the data structure
+        @param Q: a data structure storing the vertices ordered by minimum distance
+        @param d: a mapping of vertices to their current known shortest distance from a source node
+                  (source node has default distance of 0 from itself)
+        @return: the vertex of minimum distance from the start/source node
+        """
+        minDist, minNodeInd = float('inf'), None
+        for i, v in enumerate(pq):
+            if d[v] <= minDist:
+                minDist = d[v]
+                minNodeInd = i
+        assert minNodeInd is not None
+        minNode = pq[minNodeInd]
+        del pq[minNodeInd]
+        return minNode
 
-    def __hash__(self):
-        return hash(self.val)
+    def dijkstra_SSSP(self, source):
+        """
+        Dijkstra's algorithm for single-source shortest paths, given a start and target Vertex.
+        Note: graph must not contain any cycles with negative weights (for now, just restrict the graph to be a DAG)
+        @param source: source node
+        @return: mapping of the shortest distances between source and every other vertex (default d(s, s) <- 0)
+                 and a mapping of every node to its parent in its corresponding shortest path (see: subpaths of SP's
+                 are themselves SPs, and triangle inequality for why this works)
+        """
+        self.verifyDAG(source)
+        d = {}
+        for v in self.vertices:
+            d[v] = float('inf')
+        d[source] = 0
+        visited = set()
+        priority_queue = list(self.vertices)  # This can be changed to be a heapq later
+        parentMap = {source: source}
 
-    def __eq__(self, other):
-        return isinstance(other, Vertex) and other.val == self.val
+        while priority_queue:
+            u = Graph.minDistVertex(priority_queue, d)
+            visited.add(u)
+            for v in self.getChildren(u):
+                flag = self.relax(u, v, d)
+                if flag:
+                    parentMap[v] = u
+
+        return d, parentMap
+
+    def printSSSPs(self, desiredVertices=None):
+        """
+        Prints the SSSP from source to each vertex in desiredVertices. Uses Dijkstra's Algorithm for SSSP.
+        @param desiredVertices: if None, then assume all vertices are desired
+        """
+        if desiredVertices is None:
+            desiredVertices = self.getVertices()
+        assert all(isinstance(v, Vertex) for v in desiredVertices)
+        d, p = self.dijkstra_SSSP(a)
+        for v in desiredVertices:
+            if v not in p or v not in desiredVertices:
+                continue
+            i, path = v, [v]
+            while i != a:  # Potentially O(V) loop here
+                i = p[i]
+                path.append(i)
+
+            print("SSSP from source %r to node %r has distance %r and path %r" % (a, v, d[v], path[::-1]))
 
 
 if __name__ == "__main__":
-    pass
+    a, b, c, d, e = Vertex("a"), Vertex("b"), Vertex("c"), Vertex("d"), Vertex("e")
+    G = Graph()
+    G.addEdge(a, b, 2)
+    G.addEdge(a, c, 3)
+    G.addEdge(c, b, 5)
+    G.addEdge(b, d, 4)
+    G.addEdge(c, e, 5)
+    G.addEdge(d, e, 4)
+
+    G.printSSSPs()

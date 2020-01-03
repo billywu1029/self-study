711d2d96535b77f2dc20695e01f24cdca66451b0
Bill Wu <billwu@mit.edu>
2020-01-03 14:42:55 -0500

    Added Bellman Ford + refactoring

--- Graph.py
+++ Graph.py
@@ -147,7 +147,7 @@ class Graph:
         return path[::-1]  # Reverse path so that it is from start to target
 
     # Should this be a static method or not??
-    def relax(self, u, v, d):
+    def relax(self, u, v, d, p=None, pq=None, curr_d=None):
         """
         If current "shortest" distance from s to v is greater than shortest distance from s to u + w(u,v), then set
         the new shortest distance from s to v to be = newly found shortest distance d(s,u) + w(u,v).
@@ -162,18 +162,29 @@ class Graph:
                 |  /
                 | /
                 u
+        Additional params pq and curr_d used by Dijkstra's to update the vertex heap
+        p used by both Dijkstra's and Bellman Ford, etc to find shortest path by following predecessors to source
+        @param u first input vertex
+        @param v second input vertex
+        @param d current shortest path distances mapping
+        @param p predecessor mapping, default to None
+        @param pq priority queue of vertices ordered by key=distance
+        @param curr_d current distance away from source
         """
         assert u in d and v in d and u in self.edges and v in self.edges[u]
-        flag = d[v] > d[u] + self.edges[u][v]
-        if flag:
+        if d[v] > d[u] + self.edges[u][v]:
             d[v] = d[u] + self.edges[u][v]
-        return flag
+            if p is not None:
+                p[v] = u
+            if pq is not None:
+                assert curr_d is not None
+                heapq.heappush(pq, (curr_d + self.edges[u][v], v))
 
     def verifyDAG(self, s):
         """
         In order for the SSSP by topological sort to work, ensure that this graph is a directed acyclic graph (DAG),
         for the nodes reachable by s
-        :param s specifying source node s to make traversal start easier
+        @param s specifying source node s to make traversal start easier
         """
         # Find presence of a back edge, if none, then the nodes reachable from s + edges traversed form a DAG
         curr_nodes = set()
@@ -197,8 +208,8 @@ class Graph:
         -> O((|E| + |V|) log |V|)
             since the heappush for decrease key and heappop's readjusting after extracting min are both O(log |V|). 
         @param source: source node
-        @return: mapping of the shortest distances between source and every other vertex (default d(s, s) <- 0)
-                 and a mapping of every node to its parent in its corresponding shortest path (see: subpaths of SP's
+        @return: 1. mapping of the shortest distances between source and every vertex (default d(s, s) <- 0)
+                 2. mapping of every node to its parent in its corresponding shortest path (see: subpaths of SP's
                  are themselves SPs, and triangle inequality for why this works)
         """
         self.verifyDAG(source)
@@ -216,31 +227,91 @@ class Graph:
                 continue
             visited.add(u)
             for v in self.getChildren(u):
-                flag = self.relax(u, v, d)
-                if flag:
-                    parentMap[v] = u
-                    heapq.heappush(priority_queue, (curr_d + self.edges[u][v], v))
+                self.relax(u, v, d, parentMap, priority_queue, curr_d)
 
         return d, parentMap
 
-    def printSSSPs(self, desiredVertices=None):
+    def bellmanFord_SSSP(self, source):
+        """
+        Bellman-Ford algorithm for single source shortest paths. Detects negative cycles in addition to providing
+        the cycle itself if applicable, o/w returns the mapping of the sp distance from source to each vertex,
+        and the predecessor mapping to help construct shortest paths.
+        Runtime complexity: O(|V||E|) since we need to iterate over every vertex and for each, relax each edge.
+        Correctness: We need this VE loop to guarantee that every edge is optimally relaxed. Each outer iteration
+            only reduces the shortest path distance from source to each vertex. At iteration i of this loop, we know
+            that d[v] is at most the weight of any path from source to v using at most i edges:
+                d[v] <= minWeight(p) : len(p) <= i-1
+            where p is a given path. We also know that if there is no negative cycle, then the shortest path will have
+            at most |V| vertices, ie |V*| <= |V|.
+            A negative cycle must also be found (and no false positives either) since it only identifies the cycle if
+            for some vertex u,
+        @param source: input source node
+        @return: 1. A negative cycle if it exists, as a list of vertices. None o/w
+                 2. Mapping of the shortest distances between source and every vertex. None if negative cycle exists.
+                 3. Mapping of predecessors, None if negative cycle exists
+        """
+        d, p = {}, {}  # Initialize sp distances mapping and predecessor mapping
+        for v in self.vertices:
+            d[v] = float('inf')
+        d[source] = 0
+
+        for _ in self.vertices:
+            for u in self.edges:
+                for v in self.edges[u]:
+                    self.relax(u, v, d, p)
+
+        for u in self.edges:
+            for v in self.edges[u]:
+                if d[v] > d[u] + self.getWeight(u, v):
+                    return self.getCycle(v, p), None, None
+
+        return None, d, p
+
+    def getCycle(self, v, p):
+        """
+        Assuming that a negative weight cycle exists in the graph, return it by following the predecessors until
+        a seen vertex along the predecessor path is seen again. Output vertices only on the cycle itself (without any
+        of the vertices used to reach the cycle).
+        @param v: Input vertex where cycle was detected via Bellman Ford, not necessarily already on the cycle path
+        @param p: predecessor mapping
+        @return: list of vertices in the cycle
+        """
+        seen, cycle = set(), []
+        nextNode = v
+        # No infinite loop if there isn't a cycle, since p[source] = source, just garbage return value
+        while nextNode not in seen:
+            seen.add(nextNode)
+            cycle.append(nextNode)
+            nextNode = p[nextNode]
+        lastNode = nextNode
+
+        # If reached the original v, then the following does nothing. o/w we delete all vertices needed to reach a
+        # vertex in the cycle by identifying the index where the first cycle vertex occurred in our predecessor walk
+        firstOccurIdx = cycle.index(lastNode)  # Gets the index of the first occurrence of the lastNode Vertex
+        del cycle[:firstOccurIdx]  # Removes all vertices used to reach cycle, ie up to but not including firstOccurIdx
+
+        return list(reversed(cycle))  # Reversed since we followed predecessors backwards to identify cycle
+
+    def printSSSPs(self, source, desiredVertices=None):
         """
         Prints the SSSP from source to each vertex in desiredVertices. Uses Dijkstra's Algorithm for SSSP.
+        @param source input source node
         @param desiredVertices: if None, then assume all vertices are desired
         """
         if desiredVertices is None:
             desiredVertices = self.getVertices()
         assert all(isinstance(v, Vertex) for v in desiredVertices)
-        d, p = self.dijkstra_SSSP(a)
+        d, p = self.dijkstra_SSSP(source)
+        # _, d, p = self.bellmanFord_SSSP(source)
         for v in desiredVertices:
             if v not in p or v not in desiredVertices:
                 continue
             i, path = v, [v]
-            while i != a:  # Potentially O(V) loop here
+            while i != source:  # Potentially O(V) loop here
                 i = p[i]
                 path.append(i)
 
-            print("SSSP from source %r to node %r has distance %r and path %r" % (a, v, d[v], path[::-1]))
+            print("SSSP from source %r to node %r has distance %r and path %r" % (source, v, d[v], path[::-1]))
 
 
 if __name__ == "__main__":
@@ -253,4 +324,14 @@ if __name__ == "__main__":
     G.addEdge(c, e, 5)
     G.addEdge(d, e, 4)
 
-    G.printSSSPs()
+    G.printSSSPs(a)
+
+    cycleG = Graph()
+    cycleG.addEdge(a, b, 2)
+    cycleG.addEdge(d, a, 2)
+    cycleG.addEdge(a, c, -1)
+    cycleG.addEdge(c, e, -2)
+    cycleG.addEdge(e, a, 1)
+
+    result = cycleG.bellmanFord_SSSP(d)
+    print(result)

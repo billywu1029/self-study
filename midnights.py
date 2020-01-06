f7a5a40029b1814a9b090880d1603b78df03c0c8
Bill Wu <billwu@mit.edu>
2020-01-06 00:56:19 -0500

    Code to construct flow network + heuristics functions and constants added

--- midnights.py
+++ midnights.py
@@ -11,19 +11,38 @@ from FlowNetwork import *
 import json
 
 POINTS_REQ = 50
+PERSON_BASE_COST = 10
+PREFER_DAY_WEIGHT = 20
+PREFER_MIDNIGHT_WEIGHT = 5
+PREFERENCE_OFFSET = 26  # To make costs positive, ie offset the potential -10 - 2 for c(boi, m)
+STRONGLY_AGAINST_PENALTY = 50
 
-def weightedMidnightsCount(totalMidnights: int, numPeople: int, pointsProgress: int) -> int:
+def weightedPersonCost(pointsProgress: int) -> int:
     """
-    Gets an upper bound on the number of midnights to be assigned to a person, determined by:
-        floor(totalMidnights/numPeople) weighted by the person's pointsProgress
-    @param totalMidnights: number of required midnights for the week
-    @param numPeople: number of people available to do midnights for the week
+    Gets a weighted cost of assigning a midnight to each person, determined by:
+        PERSON_BASE_COST ** (1 + person's pointsProgress / POINTS_REQ)
+    Intuition: The more midnight points someone has, the less midnights they should be assigned, and vice versa.
     @param pointsProgress: number of midnight points the person has
     @return: int of max num of midnights one person should be allowed in a week
     """
-    return int(round(totalMidnights // numPeople * (1 - pointsProgress / POINTS_REQ))) + 1
+    return int(round(PERSON_BASE_COST ** (1 + pointsProgress / POINTS_REQ)))
 
-def getMidnightAssignments(G: Network, people: list) -> dict:
+def getCostBoiToMidnight(preferDay: bool, preferMidnight: bool) -> int:
+    """
+    Gets the cost for an edge from a person to any corresponding midnight. Intuitively, should assign low cost for
+    midnights that are more preferred, and higher cost if not preferred. Can even use different levels of preference
+    so that strongly prefer -> 0 cost, strongly do not prefer -> super high cost.
+    @param preferDay: True to indicate that the day is preferred, False o/w
+    @param preferMidnight: True to indicate that the midnight is preferred, False o/w
+    @return: int generated via formula: -1(preferDay) * PREFER_DAY_WEIGHT - 1(preferMidnight) * PREFER_MIDNIGHT_WEIGHT
+    """
+    # TODO: Different levels of preference to potentially create useful backdoors to guarantee midnights you want
+    dayCoeff = PREFER_DAY_WEIGHT if preferDay else 1
+    midnightCoeff = PREFER_MIDNIGHT_WEIGHT if preferMidnight else 1
+    penalty = -STRONGLY_AGAINST_PENALTY if not preferMidnight and not preferDay else 1
+    return PREFERENCE_OFFSET - (dayCoeff + midnightCoeff) * penalty
+
+def getMidnightAssignments(G: FlowNetwork, people: list) -> dict:
     """
     Gets the mapping of midnight assignments for each person according to a Flow Network G.
     Assumes that max flow has already been found in G, behavior unspecified o/w.
@@ -34,7 +53,7 @@ def getMidnightAssignments(G: Network, people: list) -> dict:
     result = {}
     for boi in people:
         u = Vertex(boi)
-        result[boi] = [m.val for m in G.getChildren(u) if G.flowGraph.getWeight(u, m) > 0]
+        result[boi] = [m.val for m in G.capacityGraph.getChildren(u) if G.flowGraph.getWeight(u, m) > 0]
     return result
 
 def createNewMidnight(day: str, m: str, i: int) -> str:
@@ -56,6 +75,7 @@ def createNewMidnight(day: str, m: str, i: int) -> str:
 # "midnightPreferences": {Bill: [bathrooms, dinings, ...], Jack: [commons, dishes, ...], ...}
 # "progress": {Bill: 21, Jack: 35, ...}  // Number of midnights points each person has
 with open("midnights.json", "r") as infile:
+    # TODO: Add point values to each midnight and let the cost generation reflect the point reward
     info = json.load(infile)
     dayToMidnights = info["dayToMidnights"]
     midnightsToNumReq = info["midnightsToNumReq"]
@@ -65,7 +85,8 @@ with open("midnights.json", "r") as infile:
     progress = info["progress"]
 
     S, T = Vertex("S"), Vertex("T")
-    G = Network(S, T)
+    G = FlowNetwork(S, T)
+    v = {}  # Stores the mapping of string to Vertex wrapped string (used to identify/add edges to/from bois in graph)
 
     midnightsCount = 0
     for day in dayToMidnights:
@@ -73,19 +94,23 @@ with open("midnights.json", "r") as infile:
             midnightsCount += 1
 
     for boi in people:
+        v[boi] = Vertex(boi)
         # Edges from source to people with weight: w(S, p) = ceil(floor(n/k) * (progress[p]/total_req))
-        # G.addEdge(S, boi, weightedMidnightsCount(midnightsCount, len(people), progress[boi]))
-        G.addEdge(S, boi, 100000)
-        for day in dayToMidnights:
-            for m in dayToMidnights[day]:
-                for i in range(midnightsToNumReq[m]):
-                    midnightWithDay = createNewMidnight(day, m, i)
-                    # Edges from people to midnights with weight: w(p, m) = 1(m in mPref[p]) * 1(day in dayPref[p])
-                    if day in dayPreferences[boi] and m in midnightPreferences[boi]:
-                        G.addEdge(boi, midnightWithDay, 1)
-                    # Edges from midnights to sink with weight: w(m, T) = choresToNumReq[m]
-                    G.addEdge(midnightWithDay, T, 1)
+        G.addEdge(S, v[boi], 5, weightedPersonCost(progress[boi]))
 
-    print(G.getMaxFlow())
+    for day in dayToMidnights:
+        for m in dayToMidnights[day]:
+            for i in range(midnightsToNumReq[m]):
+                midnightWithDay = Vertex(createNewMidnight(day, m, i))
+                # Edges from midnights to sink with weight 1, cost 1
+                G.addEdge(midnightWithDay, T, 1, 1)
+                for boi in people:
+                    # Exists an edge from every boi to every midnight, different cost based on day + m preferences
+                    costBoiMidnight = getCostBoiToMidnight(day in dayPreferences[boi], m in midnightPreferences[boi])
+                    if day in dayPreferences[boi] or m in midnightPreferences[boi]:
+                        G.addEdge(v[boi], midnightWithDay, 1, costBoiMidnight)
+
+    # print(G.getMaxFlow())
+    print(G.getMinCostMaxFlow())
     peopleMidnightMap = getMidnightAssignments(G, people)
     print(peopleMidnightMap)

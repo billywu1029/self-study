25241bef3023059630c9ab212c27159e384c2f48
Bill Wu <billwu@mit.edu>
2020-01-12 22:44:05 -0500

    Fixes + refactoring + more heuristics

--- tests/midnights.py
+++ tests/midnights.py
@@ -10,13 +10,20 @@ Date: 12/30/19
 from FlowNetwork import *
 import json
 
-POINTS_REQ = 50
+POINTS_REQ = 59
 PERSON_BASE_COST = 10
 # PREFER_DAY_PENALTY = 30
 PREFER_MIDNIGHT_PENALTY = 10
 STRONGLY_AGAINST_PENALTY = 50  # Could potentially be used later when implementing multiple levels of preferences
+NEED_MIDNIGHT_REWARD = -50  # Reduce cost if really need this midnight and boi can do the midnight
+LIMITED_OPTIONS_REWARD = -40  # Reduce cost if boi can only do limited amount of midnights and boi can do the midnight
+LIMITED_OPTIONS_MIDNIGHTS_REQ = 2  # max number of midnights one can pref to qualify as having "limited options"
+# Penalty for someone taking a higher point value midnight when they have higher point progress
+POINT_FAIRNESS_PENALTY_MULTIPLIER = 5
+MIDNIGHTS_PER_DAY_LIMIT = 1
+MIDNIGHTS_PER_WEEK_LIMIT = 3
 
-def weightedPersonCost(pointsProgress: int) -> int:
+def weightedPersonCost(pointsProgress: float) -> int:
     """
     Gets a weighted cost of assigning a midnight to each person, determined by:
         PERSON_BASE_COST ** (1 + person's pointsProgress / POINTS_REQ)
@@ -45,19 +52,20 @@ def getMidnightAssignments(G: FlowNetwork, people: list) -> dict:
                     result[boi].append(m.val)
     return result
 
-def createNewMidnight(day: str, m: str, i: int) -> str:
+def createNewMidnightVertex(day: str, m: str, i: int) -> Vertex:
     """
-    Create a new string that concatenates all three pieces of information together: day, midnight, and midnight number
+    Create a new Vertex with string val that concatenates all three pieces of information together:
+        day, midnight, and midnight number
     @param day: which day the midnight is assigned
     @param m: the midnight
     @param i: the midnight number, ie waitings 1 or waitings 2 would have i=1 and i=2 respectively
-    @return: str that has all the information concatenated in a separable fashion
+    @return: Vertex with str val that has all the information concatenated in a separable fashion
     """
-    return "%s|%s|%r" % (day, m, i)
+    return Vertex("%s|%s|%r" % (day, m, i))
 
-def createNewDayNode(day: str, boi: str) -> str:
-    """Create a new string that concatenates day and boi together"""
-    return "%s|%s" % (day, boi)
+def createNewDayVertex(day: str, boi: str) -> Vertex:
+    """Create a new Vertex containing a str value that concatenates day and boi together"""
+    return Vertex("%s|%s" % (day, boi))
 
 # def getDayCost(preferDay: bool) -> int:
 #     """
@@ -67,28 +75,35 @@ def createNewDayNode(day: str, boi: str) -> str:
 #     """
 #     return PREFER_DAY_PENALTY if not preferDay else 0
 
-def getCostBoiDayToMidnight(preferMidnight: bool, midnightWorth: int, boiProgress: int) -> int:
+def getCostBoiDayToMidnight(preferMidnight: bool, midnightVal: int, boiPoints: int, numPref: int, numAvail: int) -> int:
     """
     Gets the cost for an edge from a person to any corresponding midnight. Intuitively, should assign low cost for
     midnights that are more preferred, and higher cost if not preferred. Can even use different levels of preference
     so that strongly prefer -> 0 cost, strongly do not prefer -> super high cost.
     @param preferMidnight: True to indicate that the midnight is preferred, False o/w
-    @param midnightWorth: How many points the midnight is worth
-    @param boiProgress: How many points the person has
+    @param midnightVal: How many points the midnight is worth
+    @param boiPoints: How many points the person has
+    @param numPref: Number of people who pref'ed the midnight
+    @param numAvail: Total number of midnights that the current person can do/number of midnights pref'ed
     @return: int generated via formula:
         1(preferMidnight) * PREFER_MIDNIGHT_PENALTY
     """
     # TODO: Different levels of preference to potentially create useful backdoors to guarantee midnights you want
-    # TODO: point values of midnights to influence cost, dpeneding on progress made
-    midnightCoeff = PREFER_MIDNIGHT_PENALTY if not preferMidnight else 1
-    return midnightCoeff
+    midnightPrefPenalty = STRONGLY_AGAINST_PENALTY if not preferMidnight else 1
+    # If only 1 person can do the midnight, then reduce the cost dramatically for the one that can do it
+    midnightNeededReward = NEED_MIDNIGHT_REWARD if preferMidnight and numPref == 1 else 0
+    # If the person can only do 1 or 2 midnights, then reduce the cost if the midnight is one they actually can do
+    # eg. poker/sports/adt bois only get back really late so they can only do like commons or sth easy later at night
+    limitedOptionsReward = LIMITED_OPTIONS_REWARD if preferMidnight and numAvail <= LIMITED_OPTIONS_MIDNIGHTS_REQ else 0
+    return midnightPrefPenalty + midnightNeededReward + limitedOptionsReward \
+           + int(midnightVal ** 2 * boiPoints / POINTS_REQ * POINT_FAIRNESS_PENALTY_MULTIPLIER)
 
 def getPeopleMidnightsToDayAssignments(peopleTasksMap: dict) -> dict:
     """
     Given a mapping of people to their list of midnights, return a new map that maps each day to the corresponding
     people that are in turn mapped to whichever midnights they were assigned to do for that particular day.
     """
-    result = {"M": {}, "T": {}, "W": {}, "Th": {}, "F": {}, "Sa": {}, "Su": {}}
+    result = {"Monday": {}, "Tuesday": {}, "Wednesday": {}, "Thursday": {}, "Friday": {}, "Saturday": {}, "Sunday": {}}
     for boi in peopleTasksMap:
         for m in peopleTasksMap[boi]:
             day, midnight, idx = m.strip().split("|")  # Stay safe by stripping, remember that kids
@@ -139,7 +154,6 @@ def extractData(inPath: str) -> tuple:
         progress = info["progress"]
     return dayToMidnights, midnightPointValues, midnightsToNumReq, people, dayPreferences, midnightPreferences, progress
 
-
 def generateMidnightsFlowNetwork(dayToMidnights: dict,
                                  midnightPointValues: dict,
                                  midnightsToNumReq: dict,
@@ -158,7 +172,7 @@ def generateMidnightsFlowNetwork(dayToMidnights: dict,
     @param dayPreferences: Midnight preferences for which days are best
     @param midnightPreferences: Which midnights each person prefers
     @param progress: Number of midnights points each person has
-    @param outPath: output file str, if doesn't exist, then creates the file under the tests/ directory.
+    @param outPath: (optional) output file str, if doesn't exist, then creates the file under the tests/ directory.
         Follows format specified in FlowNetwork's serializeJSON method
     @return: Flow Network that models the midnights assignment problem
     """
@@ -168,28 +182,42 @@ def generateMidnightsFlowNetwork(dayToMidnights: dict,
 
     for boi in people:
         v[boi] = Vertex(boi)
-        # Edges from source to people with weight: w(S, p) = ceil(floor(n/k) * (progress[p]/total_req))
-        G.addEdge(S, v[boi], 4, weightedPersonCost(progress[boi]))  # Total limit per week to be 4
+        # Edges from source to people limited by number of midnights per week, and cost based on midnight point progress
+        G.addEdge(S, v[boi], MIDNIGHTS_PER_WEEK_LIMIT, weightedPersonCost(progress[boi]))
+
+    for boi in people:
+        for boisDay in dayToMidnights:  # all 7 days for each boi to capture midnights/day limit
+            # Limit by day preferences, ie only days pref'ed will have an edge from the boi -> day
+            if boisDay in dayPreferences[boi]:
+                dayWithBoi = createNewDayVertex(boisDay, boi)
+                G.addEdge(v[boi], dayWithBoi, MIDNIGHTS_PER_DAY_LIMIT, 1)
+
+    # Gather midnight preference counts per midnight, so as to weight midnights that can only be done by 1 person
+    # very negative, so that they are guaranteed to be assigned to them
+    midnightsPrefCountMap = {}
+    for boi in midnightPreferences:
+        for midnight in midnightPreferences[boi]:
+            midnightsPrefCountMap[midnight] = midnightsPrefCountMap.get(midnight, 0) + 1
 
     for day in dayToMidnights:
         for m in dayToMidnights[day]:
             for i in range(midnightsToNumReq[m]):
-                midnightWithDay = Vertex(createNewMidnight(day, m, i))
+                midnightWithDay = createNewMidnightVertex(day, m, i)
                 # Edges from midnights to sink with weight 1, cost 1
                 G.addEdge(midnightWithDay, T, 1, 1)
                 for boi in people:
-                    for boisDay in dayToMidnights:  # all 7 days for each boi to capture midnights/day limit
-                        if boisDay in dayPreferences[boi]:
-                            dayWithBoi = Vertex(createNewDayNode(boisDay, boi))
-                            # limit number of midnights per day to 1, and higher cost if day not preferred
-                            G.addEdge(v[boi], dayWithBoi, 1, 1)
-                            # Edge from every boi's day to every midnight pref'ed, cost depends on progress
-                            costBoiDayToMidnight = getCostBoiDayToMidnight(
-                                m in midnightPreferences[boi],
-                                midnightPointValues[m],
-                                progress[boi]
-                            )
-                            G.addEdge(dayWithBoi, midnightWithDay, 1, costBoiDayToMidnight)
+                    # Limit by midnight preferences, ie only midnights pref'ed will have an edge from boiDay -> midnight
+                    if m in midnightPreferences[boi]:
+                        dayWithBoi = createNewDayVertex(day, boi)
+                        # Edge from every boi's day to every midnight pref'ed, cost depends on progress
+                        costBoiDayToMidnight = getCostBoiDayToMidnight(
+                            m in midnightPreferences[boi],
+                            midnightPointValues[m],
+                            progress[boi],
+                            midnightsPrefCountMap[m],
+                            len(midnightPreferences[boi])
+                        )
+                        G.addEdge(dayWithBoi, midnightWithDay, 1, costBoiDayToMidnight)
 
     if outPath is not None:
         G.serializeToJSON(outPath)
@@ -198,8 +226,9 @@ def generateMidnightsFlowNetwork(dayToMidnights: dict,
 
 
 if __name__ == "__main__":
-    inpPath = "midnights.json"
-    dayToMidnights, midnightPointValues, midnightsToNumReq, people, dayPreferences, midnightPreferences, progress = extractData(inpPath)
+    inpPath = "midnightsBigTest.json"
+    dayToMidnights, midnightPointValues, midnightsToNumReq, people, dayPreferences, midnightPreferences, progress = extractData(
+        inpPath)
     G = generateMidnightsFlowNetwork(dayToMidnights, midnightPointValues, midnightsToNumReq, people, dayPreferences,
                                      midnightPreferences, progress)
     cost, maxFlow = G.getMinCostMaxFlow()

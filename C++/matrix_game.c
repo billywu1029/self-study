74baaba083b39f4e0c7478dbe49291690e0c2282
Bill Wu <billwu@mit.edu>
2020-06-07 17:22:49 -0400

    matrix game int array, file input

---
+++ C++/matrix_game.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_NUM_ROWS 50
+#define MAX_NUM_COLS 50
+
+typedef struct Matrix {
+    int m, n;
+    int rows[MAX_NUM_ROWS];
+    int cols[MAX_NUM_COLS];
+} Matrix;
+
+void clear_matrix(Matrix* A) {
+    for (int i = 0; i < MAX_NUM_ROWS; i++) {
+        A->rows[i] = 0;
+    }
+    for (int j = 0; j < MAX_NUM_COLS; j++) {
+        A->cols[j] = 0;
+    }
+}
+
+void read_matrix(FILE* fp, Matrix* A) {
+    fscanf(fp, "%d %d", &A->m, &A->n);
+    for (int i = 0; i < A->m; i++) {
+        for (int j = 0; j < A->n; j++) {
+            int x;
+            fscanf(fp, "%d", &x);
+            // Don't care if redundant assignment
+            A->rows[i] |= x;
+            A->cols[j] |= x;
+        }
+    }
+}
+
+void print_matrix(Matrix* A) {
+    printf("Row marker array: \n");
+    for (int i = 0; i < A->m; i++) {
+        printf("%d ", A->rows[i]);
+    }
+    printf("\n");
+    printf("Column marker array: \n");
+    for (int j = 0; j < A->n; j++) {
+        printf("%d ", A->cols[j]); 
+    }
+    printf("\n");
+}
+
+char* game_winner(Matrix* A) {
+    static char* name[2] = {"Ashish", "Vivek"};
+    // Assuming Ashish always goes first
+    int turn = 1; // 1 if Ashish's turn, 0 if Vivek's
+    while (1) {
+        int found_row = 0;
+        for (int i = 0; i < A->m; i++) {
+            // Find the first available row
+            if (!A->rows[i]) {
+                A->rows[i] = 1;
+                found_row = 1;
+                break;
+            }
+        }
+        if (!found_row) {
+            return name[turn];
+        }
+        
+        int found_col = 0;
+        for (int j = 0; j < A->n; j++) {
+            // Find the first available row
+            if (!A->cols[j]) {
+                A->cols[j] = 1;
+                found_col = 1;
+                break;
+            }
+        }
+        if (!found_col) {
+            return name[turn];
+        }
+        turn ^= 1;
+    }
+}
+
+int main() {
+    FILE* infile = fopen("matrix_game.in", "r");
+    if (infile == NULL) {
+        printf( "Error: could not open matrix_game.in\n" );
+        exit(EXIT_FAILURE);
+    }
+
+    int num_matrices;
+    fscanf(infile, "%d", &num_matrices);
+    Matrix matrix;
+
+    for (int i = 0; i < num_matrices; i++) {
+        clear_matrix(&matrix);
+        read_matrix(infile, &matrix);
+        // print_matrix(&matrix);
+        char* answer = game_winner(&matrix);
+        printf("%s\n", answer);
+    }
+}

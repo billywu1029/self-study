0edc34235474ae4b8ddb278927f6a4558cd6504d
Bill Wu <billwu@mit.edu>
2020-08-16 13:09:58 -0700

    Shuffle + map and reduce call, test works

--- Python/MapReduce.py
+++ Python/MapReduce.py
@@ -7,6 +7,16 @@ class SimpleMapReduce:
         self.map_func = map_func
         self.reduce_func = reduce_func
 
+    def shuffle(self, map_outputs):
+        """Organize and return map outputs by key: list of (k, list<v>) pairs."""
+        shuffled = {}
+        for k, v in map_outputs:
+            if k not in shuffled:
+                shuffled[k] = [v]
+            else:
+                shuffled[k].append(v)
+        return shuffled.items()
+
     def process_inputs(self, infile):
         self.__call__(infile)
 
@@ -16,7 +26,12 @@ class SimpleMapReduce:
         shuffle the mapped values to be organized by key, and then call the reduce function
         to reduce the organized map outputs to produce <k2, v2> pairs. Return the result.
         """
-        raise NotImplementedError
+        map_outputs = self.map_func(infile)
+        shuffled = self.shuffle(map_outputs)
+        reduce_outputs = []
+        for item in shuffled:
+            reduce_outputs.append(self.reduce_func(item))
+        return reduce_outputs
 
 def file_to_words(filename: str) -> list:
     """Reads in a file, and returns a list of (word, count) pairs."""
@@ -40,21 +55,22 @@ def count_words(map_output: list) -> tuple:
 if __name__ == "__main__":
     # Testing file_to_words and count_words:
     input_filename = "mapreduce.in"
-    results = file_to_words(input_filename)
-    print(results)
-    counts = {}
-    for word, c in results:
-        if word not in counts:
-            counts[word] = [c]
-        else:
-            counts[word].append(c)
-
-    reduce_results = []
-    for word in counts:
-        reduce_results.append(count_words([word, counts[word]]))
-    reduce_results.sort(key=lambda x: x[1], reverse=True)
-    print(reduce_results)
+    # results = file_to_words(input_filename)
+    # print(results)
+    # counts = {}
+    # for word, c in results:
+    #     if word not in counts:
+    #         counts[word] = [c]
+    #     else:
+    #         counts[word].append(c)
+    #
+    # reduce_results = []
+    # for word in counts:
+    #     reduce_results.append(count_words([word, counts[word]]))
+    # reduce_results.sort(key=lambda x: x[1], reverse=True)
+    # print(reduce_results)
 
     mr = SimpleMapReduce(file_to_words, count_words)
-    # results = mr(input_filename)
-    # print(results)
+    results = mr(input_filename)
+    results.sort(key=lambda x: x[1], reverse=True)
+    print(results)

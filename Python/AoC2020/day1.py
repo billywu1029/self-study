f8d62277bc0b4b90af1c5401a9036ca3af1ca520
Bill Wu <billwu@mit.edu>
2020-12-20 20:19:19 -0500

    day 1 part 2

--- Python/AoC2020/day1.py
+++ Python/AoC2020/day1.py
@@ -1,7 +1,18 @@
 with open("day1.in", "r") as f:
-  nums = set(map(int, f.read().strip().split("\n")))
+  nums = list(map(int, f.read().strip().split("\n")))
+  numset = set(nums)
+  
+  # Two-sum
   for i in nums:
-    if 2020-i in nums:
+    if 2020-i in numset:
       print(i, 2020-i, i * (2020-i))
       break
   
+  # Three-sum
+  for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+      x = 2020 - nums[i] - nums[j]
+      if x in numset: 
+        print(nums[i], nums[j], x, nums[i] * nums[j] * x)
+        exit()
+
